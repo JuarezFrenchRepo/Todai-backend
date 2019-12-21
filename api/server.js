67eb3ee2683932f25c378f4389ec75bafeadf67a
juarezfrench <markedfrench@gmail.com>
2019-12-20 23:27:30 -0600

    Initial setup

---
+++ api/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// const authenticate = require('../auth/authenticate-middleware.js');
+// const authRouter = require('../auth/auth-router.js');
+// const jokesRouter = require('../jokes/jokes-router.js');
+
+const server = express();
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "server is up", dbenv: process.env.DB_ENV });
+  });
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+// server.use('/api/auth', authRouter);
+// server.use('/api/users', authRouter);
+// server.use('/api/jokes', authenticate, jokesRouter);
+
+module.exports = server;
---
+++ knexfile.js
@@ -0,0 +1,41 @@
+module.exports = {
+    development: {
+      client: 'sqlite3',
+      connection: { filename: './database/auth.db3' },
+      useNullAsDefault: true,
+      migrations: {
+        directory: './database/migrations',
+        tableName: 'dbmigrations',
+      },
+      seeds: { directory: './database/seeds' },
+    },
+  
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  
+  
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/auth.db3",
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+  
+  
+  }
+  

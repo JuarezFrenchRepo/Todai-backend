4371485037eb6f344eb907ce2789fafc4cfc48b4
juarezfrench <markedfrench@gmail.com>
2020-01-06 18:22:26 -0600

    Modified CORS for acess

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+server.use(cors({credentials:true, origin:{localhost:3000} }));
 
 
 server.use('/api/auth', authRouter);
